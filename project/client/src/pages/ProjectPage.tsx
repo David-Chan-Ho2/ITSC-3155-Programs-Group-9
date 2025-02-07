00768072f645db8f7df3d96adf422a57652414b9
David-Chan-Ho2 <dho12@charlotte.edu>
2025-02-06 23:54:21 -0500

    add project page

---
+++ project/client/src/pages/ProjectPage.tsx
@@ -0,0 +1,21 @@
+import useProjects from "../hooks/useProjects"
+import IProject from "../types/projects.types"
+
+function ProjectPage() {
+    const { data, isLoading, error } = useProjects()
+
+    if (isLoading) return <p>Loading...</p>
+    if (error) return <p>Error: {error.message}</p>
+
+    return (
+        <ul>
+            {data?.map((project: IProject) => (
+                <li key={project.id}>
+                    <a href={`/projects/${project.id}`}>{project.name}</a>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export default ProjectPage
